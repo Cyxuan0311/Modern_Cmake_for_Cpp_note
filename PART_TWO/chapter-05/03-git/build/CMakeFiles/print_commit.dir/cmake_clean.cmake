84bc9f8d099600357cdc6717716949d5ae6038db
Frames <3078295834@qq.com>
2025-02-11 23:20:51 +0800

    Add the change by Frames

---
+++ PART_TWO/chapter-05/03-git/build/CMakeFiles/print_commit.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/print_commit.dir/print_commit.cpp.o"
+  "CMakeFiles/print_commit.dir/print_commit.cpp.o.d"
+  "print_commit"
+  "print_commit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/print_commit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PART_TWO/chapter-05/03-git/build/CMakeFiles/print_commit.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for print_commit.
+# This may be replaced when dependencies are built.
