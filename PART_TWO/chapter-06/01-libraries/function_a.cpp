8ae00afd1a5db5d8f7a125674de49945fc6fb3fa
Frames <3078295834@qq.com>
2025-02-16 21:44:48 +0800

    Add the file by Frames

---
+++ PART_TWO/chapter-06/01-libraries/function_a.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+
+void function_a(){
+    std::cout<<"Function A"<<std::endl;
+}
---
+++ PART_TWO/chapter-06/01-libraries/function_b.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+
+void function_b(){
+    std::cout<<"Function B"<<std::endl;
+}
